311a314a0180be5699ee27c99ab2bba08a4c0570
ClovisL <clacerd@outlook.com>
2021-04-20 19:29:02 -0400

    Initial code

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,6 @@
+#  Add your API key
+NYT_key = "thAhNlnvLowWF7o7jKhnJpPuLCbrdwSi"
+omdb_key = "df7f4ccf"
+weather_api_key = "de94642e731ac12e4daf62d6eb5bb59d"
+census_key = "e1e7d456035f97c91649ba503aa72f77456fadb8"
+g_key = "AIzaSyBrC6P7JC1yvuhU9717qTTG-xF75-iv_rg"
